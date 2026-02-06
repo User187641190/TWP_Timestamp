0bd69e813c7195c3fb31e0fed33e0bd1c3a1ece9
Nattapong <joyryder859@gmail.com>
2026-02-07 02:17:20 +0700

    main

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# URL สำหรับเชื่อมต่อ Database
+# ถ้าใช้ SQLite (ไฟล์อยู่ในเครื่อง) ให้ใช้บรรทัดล่างนี้:
+SQLALCHEMY_DATABASE_URL = "oracle+oracledb://Backend:BackendPass@localhost:1521/?service_name=FREEPDB1"
+
+# ถ้าใช้ Oracle ต้องแก้เป็น: "oracle+cx_oracle://user:password@host:port/sid"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL # check_same_thread ใช้เฉพาะกับ SQLite
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
---
+++ test_db.py
@@ -0,0 +1,20 @@
+import sys
+from sqlalchemy import create_engine, text
+
+# ลอง Connection String ที่เราแก้ล่าสุด
+# ตรวจสอบ User/Pass/ServiceName ให้ตรงเป๊ะๆ
+DATABASE_URL = "oracle+oracledb://Backend:BackendPass@localhost:1521/?service_name=FREEPDB1"
+def Test_db():
+    try:
+        print(f"🔄 Attempting to connect to: {DATABASE_URL}")
+        engine = create_engine(DATABASE_URL)
+        
+        # ลองเชื่อมต่อจริง
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT 'Hello Oracle' FROM DUAL"))
+            print(f"✅ Success! Database says: {result.scalar()}")
+            
+    except Exception as e:
+        print("\n❌ Connection Failed!")
+        print(f"Error Type: {type(e).__name__}")
+        print(f"Error Details: {e}")
